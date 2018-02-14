9d889f39d30d06ce5d3c63b5eb5aaf31c8d3952c
Balazs Salfay <balazs.salfay@gmail.com>
2018-02-14 11:51:30 +0100

    new branch

--- src/Greeting.java
+++ src/Greeting.java
@@ -1,7 +1,11 @@
 public class Greeting {
   String name;
 
-  public String greet(String name) {
-    return name;
+  public void greet(String greeting) {
+    System.out.println("Greetings for the " + greeting);
+  }
+
+  public int sum(int number1, int number2) {
+    return  number1 + number2;
   }
 }
