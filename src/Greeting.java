90b8fda7b86db6571f77f0202215d36f69a8a1ff
Balazs Salfay <balazs.salfay@gmail.com>
2018-02-14 13:28:58 +0100

    b6

--- src/Greeting.java
+++ src/Greeting.java
@@ -2,7 +2,7 @@ public class Greeting {
   String name;
 
   public void greet(String greeting) {
-    System.out.println("Greetings for the " + greeting);
+    System.out.println("Greetings for the" + greeting);
   }
 
   public int sum(int number1, int number2) {
