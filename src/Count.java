18715a1cc8c7150014a334984005a4d90bde3156
Balazs Salfay <balazs.salfay@gmail.com>
2018-02-14 11:19:01 +0100

    b5-two parameter

--- src/Count.java
+++ src/Count.java
@@ -1,9 +1,9 @@
 public class Count {
 
+  int year = 1987;
   int month = 6;
 
-
-  public void count(int month) {
-    System.out.println(month);
+  public void count(int year, int month) {
+    System.out.println(year + month);
   }
 }
