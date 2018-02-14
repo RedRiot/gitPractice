fdab2e065a40c04dbcf6bfecd6b15eeeec6480f2
Tung <tung@Macintosh-MacBook-Pro-9.local>
2018-02-14 10:24:40 +0100

    1

---
+++ src/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        printHello();
+    }
+
+    private static void printHello() {
+        System.out.println("Hello)");
+    }
+}
