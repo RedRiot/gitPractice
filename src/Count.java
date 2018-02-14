a69a82c6b7d65d7f8a5060baec457d6b22336992
Balazs Salfay <balazs.salfay@gmail.com>
2018-02-14 11:33:41 +0100

    b5 hopefully the last

--- src/Count.java
+++ src/Count.java
@@ -1,16 +1,9 @@
 public class Count {
-<<<<<<< HEAD
-
+  
   int year = 1987;
   int month = 6;
 
   public void count(int year, int month) {
     System.out.println(year + month);
-=======
-  int month = 6;
-
-  public void count(int month) {
-    System.out.println(month);
->>>>>>> 04231be71dafc08300d1c37b522a62982086d239
   }
 }
