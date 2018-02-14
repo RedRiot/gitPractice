9ff383e05f0c89b5a3be22086cf1d40486ebe29f
Vajda Lilla <vajdalil.hu@gmail.com>
2018-02-14 11:36:54 +0100

    a6

--- src/Cheer.java
+++ src/Cheer.java
@@ -2,7 +2,7 @@ public class Cheer {
   String team = "Opal";
   String language = "c++";
 
-  public void cheer(String team, String language) {
-    System.out.println(team + " " + language);
+  public void cheer(String team) {
+    System.out.println("Cheer for the " + team );
   }
 }
--- src/Future.java
+++ src/Future.java
@@ -3,7 +3,7 @@ public class Future {
   String horoscopeSign = "Capricorn";
   String chineseSign = "Sheep";
 
-  public void future(String horoscopeSign, String chineseSign) {
-    System.out.println(horoscopeSign + ", " + chineseSign);
+  public void future(String chineseSign) {
+    System.out.println("Your future looks like " + chineseSign);
   }
 }
